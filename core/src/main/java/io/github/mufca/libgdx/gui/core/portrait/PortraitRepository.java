25815d89205a4417702f4dbb7a037286f536c60e
Michał Marciniak <marciniak.michal@gmail.com>
2025-11-03 13:58:26 +0100

    fix PortraitRepository

--- core/src/main/java/io/github/mufca/libgdx/gui/core/portrait/PortraitRepository.java
+++ core/src/main/java/io/github/mufca/libgdx/gui/core/portrait/PortraitRepository.java
@@ -9,11 +9,11 @@ import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.github.benmanes.caffeine.cache.RemovalCause;
 import io.github.mufca.libgdx.datastructure.lowlevel.IdProvider;
+import io.github.mufca.libgdx.util.LogHelper;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionException;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -22,6 +22,7 @@ import java.util.concurrent.ConcurrentHashMap;
 public final class PortraitRepository {
 
     private static final int MAX_CACHE_SIZE = 70;
+    private static final String FAILED_TO_CREATE_TEXTURE = "Failed to create texture: %s";
 
     private final Map<String, Long> pathToPortraitId = new ConcurrentHashMap<>();
     private final Map<Long, Set<Long>> characterToPortraits = new ConcurrentHashMap<>();
@@ -44,18 +45,13 @@ public final class PortraitRepository {
         Long existingId = pathToPortraitId.get(file.path());
         if (existingId != null) {
             addRelation(characterId, existingId);
+            return;
         }
 
         long newId = idProvider.generateUniqueId();
 
         CompletableFuture
-            .supplyAsync(() -> {
-                try {
-                    return new Pixmap(file);
-                } catch (Exception e) {
-                    throw new CompletionException(e);
-                }
-            })
+            .supplyAsync(() -> new Pixmap(file))
             .thenAcceptAsync(pixmap -> {
                 try {
                     Texture texture = new Texture(pixmap);
@@ -67,6 +63,8 @@ public final class PortraitRepository {
                         pathToPortraitId.put(file.path(), newId);
                         addRelation(characterId, newId);
                     }
+                } catch (RuntimeException e) {
+                    LogHelper.error(this, FAILED_TO_CREATE_TEXTURE.formatted(e.getMessage()));
                 } finally {
                     pixmap.dispose(); // always dispose, even if Texture throws
                 }
